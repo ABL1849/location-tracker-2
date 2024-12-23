798e2379dabced8cf8e149691bebdb18556fa21f
abl1849 <aniketlabade8@gmail.com>
2024-12-23 09:21:08 +0530

    Updated socket logic to handle multiple devices

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const socketio = require('socket.io');
+// const server = http.createServer(app);
+// const io = socketio(server);
+// const path = require('path');
+
+
+// app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname, "public")));
+
+
+// io.on("connection", function (socket) {
+//     socket.on("send-location", function (data) {
+//         io.emit("receive-location", { id: socket.id, ...data });
+//     })
+//     console.log("Connected");
+
+//     socket.on("disconnect", function () {
+//         io.emit("user-disconnected", socket.id);
+//     })
+// })
+// app.get("/", function (req, res) {
+//     res.render("index");
+// })
+
+// // server.listen(3000);
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => console.log(`Server running on port ${port}`));
+
+
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -6,25 +38,34 @@ const server = http.createServer(app);
 const io = socketio(server);
 const path = require('path');
 
-
+// Set view engine and static files
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 
+// Socket.io connection
+io.on("connection", (socket) => {
+    console.log(`New connection: ${socket.id}`);
 
-io.on("connection", function (socket) {
-    socket.on("send-location", function (data) {
+    // Receive location and broadcast to all clients
+    socket.on("send-location", (data) => {
+        console.log(`Location received from ${socket.id}:`, data);
         io.emit("receive-location", { id: socket.id, ...data });
-    })
-    console.log("Connected");
+    });
 
-    socket.on("disconnect", function () {
+    // Handle disconnection
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
         io.emit("user-disconnected", socket.id);
-    })
-})
-app.get("/", function (req, res) {
-    res.render("index");
-})
+    });
+});
 
-// server.listen(3000);
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`Server running on port ${port}`));
+// Render index page
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,91 @@
+// const socket = io();
+
+// if (navigator.geolocation) {
+//     navigator.geolocation.watchPosition((position) => {
+//         const { latitude, longitude } = position.coords;
+//         socket.emit('send-location', { latitude, longitude });
+//     }, (error) => {
+//         console.log(error);
+//     },
+//         {
+//             enableHighAccuracy: true,
+//             maximumAge: 0,
+//             timeout: 5000,
+//         })
+// }
+
+// const map = L.map("map").setView([0, 0], 10);
+
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     attribution: "Kargokart"
+// }).addTo(map)
+
+// const markers = {}
+
+// socket.on('receive-location', (data) =>{
+//     const {id, latitude, longitude} = data;
+//     map.setView([latitude, longitude], 16);
+
+//     if(markers[id]){
+//         markers[id].setLatLng([latitude, longitude]);
+//     }
+//     else{
+//         markers[id] = L.marker([latitude, longitude]).addTo(map);
+//     }
+// })
+
+// socket.on("user-disconnected", (id) =>{
+//     if(markers[id]){
+//         map.removeLayer(markers[id]);
+//         delete markers[id];
+//     }
+// })
+
+
 const socket = io();
 
+// Geolocation tracking
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
         socket.emit('send-location', { latitude, longitude });
     }, (error) => {
-        console.log(error);
-    },
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 5000,
-        })
+        console.error("Geolocation error:", error);
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+    });
 }
 
+// Initialize map
 const map = L.map("map").setView([0, 0], 10);
-
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Kargokart"
-}).addTo(map)
+    attribution: "Location Tracker"
+}).addTo(map);
 
-const markers = {}
+// Object to manage markers for each client
+const markers = {};
 
-socket.on('receive-location', (data) =>{
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude], 16);
+// Receive and update location
+socket.on('receive-location', (data) => {
+    const { id, latitude, longitude } = data;
+    console.log(`Received location from ${id}:`, data);
 
-    if(markers[id]){
+    if (markers[id]) {
+        // Update existing marker position
         markers[id].setLatLng([latitude, longitude]);
-    }
-    else{
+    } else {
+        // Create a new marker
         markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
-})
+});
 
-socket.on("user-disconnected", (id) =>{
-    if(markers[id]){
+// Handle user disconnection
+socket.on("user-disconnected", (id) => {
+    console.log(`User disconnected: ${id}`);
+    if (markers[id]) {
         map.removeLayer(markers[id]);
         delete markers[id];
     }
-})
+});
